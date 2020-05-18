592585bea0cc35a4425d11c84bef371c2090782c
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-05-18 18:38:34 +0200

    fix: devtools test final fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,8 +5,11 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from '~/redux/reducers/rootReducer';
 import rootSaga from '~/redux/sagas/rootSaga';
 
-// TODO: fix devtools with tests bug
-const devTools = typeof window === 'object' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null;
+const devTools = process.env.NODE_ENV === 'test'
+    ? (x) => x
+    : window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
     rootReducer,
