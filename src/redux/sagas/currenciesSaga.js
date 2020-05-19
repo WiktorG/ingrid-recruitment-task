01e73c6a01f2d204390c4f371ba7596ccf486101
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-05-19 09:07:23 +0200

    feat: saga to get current currency rates update

--- src/redux/sagas/currenciesSaga.js
+++ src/redux/sagas/currenciesSaga.js
@@ -1,5 +1,9 @@
-import { put, call } from 'redux-saga/effects';
+import { put, call, select } from 'redux-saga/effects';
 import api from '~/etc/api';
+
+import {
+    currenciesSymbols as currenciesSymbolsSelector,
+} from '~/redux/selectors/currenciesSelectors';
 
 import {
     currencyRatesSuccess,
@@ -10,9 +14,17 @@ import {
 export function* currencyRatesSaga({ currencySymbol }) {
     try {
         const { data } = yield call(api.currencies.latest, currencySymbol);
-        const symbols = Object.keys(data.rates).map((symbol) => symbol);
-        yield put(setCurrenciesSymbols(symbols));
         yield put(currencyRatesSuccess(data));
+        /*
+            If statement checks whether request returns different amount of
+            symbols and if yes it updates store. I do this to avoid updating store
+            everytime user selects different currency in initial select.
+        */
+        const symbols = Object.keys(data.rates).map((symbol) => symbol);
+        const storedSymbols = yield select(currenciesSymbolsSelector);
+        if (symbols.length === storedSymbols.length) {
+            yield put(setCurrenciesSymbols(symbols));
+        }
     } catch (err) {
         yield put(currencyRatesError('Something went wrong - try again later'));
     }
