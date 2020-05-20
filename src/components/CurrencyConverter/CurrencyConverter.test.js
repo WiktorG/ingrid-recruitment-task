731ccf9e5e1563b173331fbc1b84dc9bad56f2b8
Wiktor Gała <wiktorgala@Wiktors-MacBook-Pro.local>
2020-05-20 15:42:00 +0200

    tests: currency converter tests

--- src/components/CurrencyConverter/CurrencyConverter.test.js
+++ src/components/CurrencyConverter/CurrencyConverter.test.js
@@ -22,7 +22,7 @@ test('component should render', async () => {
 test('component should calculate results when user inputs value into initial Input', async () => {
     jest.spyOn(api.currencies, 'latest').mockImplementation(() => Promise.resolve({ data: currencyValid }));
     renderWithThemeAndReduxProvider(<CurrencyConverter />);
-    
+
     const initialInputNode = screen.getByTestId('InitialInput');
     const convertedInputNode = screen.getByTestId('ConvertedInput');
 
@@ -47,3 +47,25 @@ test('component should recalculate results when user inputs value into converted
     expect(initialInputNode.value).toBe('91.02');
 });
 
+test('component should show error when server rejects request', async () => {
+    jest.spyOn(api.currencies, 'latest').mockImplementation(() => Promise.reject());
+    renderWithThemeAndReduxProvider(<CurrencyConverter />);
+
+    await expect(api.currencies.latest).toHaveBeenCalled();
+    expect(screen.getByText(/Something went wrong - try again later/)).toBeInTheDocument();
+});
+
+test('component should show error when user inputs incorrect data', async () => {
+    jest.spyOn(api.currencies, 'latest').mockImplementation(() => Promise.resolve({ data: currencyValid }));
+    renderWithThemeAndReduxProvider(<CurrencyConverter />);
+
+    await expect(api.currencies.latest).toHaveBeenCalled();
+    expect(screen.getByText(/0.219731927 PLN/)).toBeInTheDocument();
+
+    const initialInputNode = screen.getByTestId('InitialInput');
+    await initialInputNode.setAttribute('type', 'text');
+
+    await fireEvent.change(initialInputNode, { target: { value: '2dad0' } });
+
+    expect(screen.getByText(/Incorrect data in fields./)).toBeInTheDocument();
+});
